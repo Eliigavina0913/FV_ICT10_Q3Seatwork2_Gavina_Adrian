5203c618ca6fd1bdcb1f1c5b7909c0b0fb7ffb08
datboidrian <adrianerickgavina@gmail.com>
2026-01-20 09:02:30 +0800

    Refactor get_team function for eligibility check

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ from pyscript import document
 
 
 def get_team(e):
-
-    document.getElementById("print").innerHTML = ""
-    document.getElementById("image").innerHTML = ""    
+  
     
-    onlinergstr = document.querySelector("input[name='onrgstr']:checked").value
-    medclr = document.querySelector("input[name='medclr']:checked").value
-    gradelvl = int(document.getElementById("lvlslct").value)
+    onlinergstr = document.getElementById("onrgstr").checked
+    medclr = document.getElementById("medclr").checked
+    gradelvl = document.getElementById("lvlslct").value
     section = document.getElementById("sct").value
 
     if onlinergstr and medclr == True:
-        print("You are eligible to join the Intramurals!")
+       document.getElementById("print").innerHTML = f"You are eligible to join the Intramurals!<br>Grade Level: {gradelvl}<br>Section: {section}"
     else:
-        print("You are not eligible to join the Intramurals.")
+        document.getElementById("print").innerHTML = "You are not eligible to join the Intramurals."
